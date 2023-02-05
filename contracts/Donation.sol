a7dda8c2add1568ab739c77717fceb88d4fae59d
Nikola <nlukic97@gmail.com>
2023-02-05 09:40:56 +0100

    updated contract and tests

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+import "hardhat/console.sol";
+
 /// @title Contract for making donation campaigns that accept eth
 /// @author Nikola Lukic
 /// @notice Made as a task of the Solidity Bootcamp
@@ -17,39 +19,57 @@ contract Donation is Ownable {
         uint256 timeGoal;
         uint256 moneyGoal;
         address creator;
-        bool registered;
-        bool complete;
     }
 
     mapping(uint256 => Campaign) public campaigns;
     mapping(uint256 => uint256) public campaignBalances;
     mapping(uint256 => bool) public campaingLocked;
+    mapping(uint256 => bool) public campaignComplete;
 
+    bool private _campaignZeroCreated;
+
+    event CampaignLocked(uint256 id);
+    event CampaignUnlocked(uint256 id);
     event FundsWithdrawn(uint256 id, uint256 amount);
-    event NewDonation(uint256 _campaignId, uint256 _amount);
-    event NewCampaign(string _title, string _description, uint256 _timeGoal, uint256 _moneyGoal);
+    event NewDonation(uint256 campaignId, uint256 amount);
+    event NewCampaign(string title, string description, uint256 timeGoal, uint256 moneyGoal);
 
-    error unauthorized();
+    error Unauthorized();
     error ActiveCampaign();
     error NoEmptyStrings();
     error InvalidTimeGoal();
-    error campaignLocked();
+    error CampaignIsLocked();
     error InsufficientAmount();
     error CampaignIsInactive();
     error NonExistantCampaign();
+    error campaignAlreadyUnlocked();
 
     modifier noEmptyStrings(string calldata _string) {
         if (bytes(_string).length <= 0) revert NoEmptyStrings();
         _;
     }
 
+    /// @dev If campaignZero has not been created, there are no campaigns created.
+    /// Otherwise, if the entered id is greater than or equal to campaignId.current(),
+    /// it must be a non existant campaign since campaignId is incremented after
+    /// a campaign in created, waiting to be the id of a newly created campaign
     modifier registered(uint256 id) {
-        if (campaigns[id].registered == false) revert NonExistantCampaign();
+        if (id >= campaignId.current()) revert NonExistantCampaign();
         _;
     }
 
-    modifier campaignNotLocked(uint256 id) {
-        if (campaingLocked[id]) revert campaignLocked();
+    modifier onlyCreator(uint256 id) {
+        if (campaigns[id].creator != msg.sender) revert Unauthorized();
+        _;
+    }
+
+    modifier onlyCreatorOrOwner(uint256 id) {
+        if (msg.sender != campaigns[id].creator && msg.sender != owner()) revert Unauthorized();
+        _;
+    }
+
+    modifier lock(uint256 id) {
+        if (campaingLocked[id]) revert CampaignIsLocked();
         _;
     }
 
@@ -64,7 +84,7 @@ contract Donation is Ownable {
         string calldata _description,
         uint256 _deadlineInDays,
         uint256 _moneyGoal
-    ) external onlyOwner noEmptyStrings(_title) noEmptyStrings(_description) {
+    ) external noEmptyStrings(_title) noEmptyStrings(_description) {
         if (_deadlineInDays <= 0) revert InvalidTimeGoal();
         if (_moneyGoal < 1) revert InsufficientAmount();
 
@@ -73,10 +93,12 @@ contract Donation is Ownable {
             _description,
             block.timestamp + _deadlineInDays * 1 days,
             _moneyGoal,
-            msg.sender,
-            true,
-            false
+            msg.sender
         );
+
+        if (campaignId.current() == 0) {
+            _campaignZeroCreated = true;
+        }
 
         campaignId.increment();
 
@@ -86,16 +108,16 @@ contract Donation is Ownable {
     /// @notice Donate money to a campaign
     /// @dev The campaign must exist, and the donator must send at least 1 wei
     /// @param id The id of the campaign to donate eth to
-    function donate(uint256 id) external payable campaignNotLocked(id) registered(id) {
+    function donate(uint256 id) external payable lock(id) registered(id) {
         Campaign storage campaign = campaigns[id];
 
         if (msg.value <= 0) revert InsufficientAmount();
-        if (campaign.complete == true || campaign.timeGoal <= block.timestamp) revert CampaignIsInactive();
+        if (campaignComplete[id] == true || campaign.timeGoal <= block.timestamp) revert CampaignIsInactive();
 
         campaignBalances[id] += msg.value;
 
         if (campaignBalances[id] >= campaign.moneyGoal) {
-            campaign.complete = true;
+            campaignComplete[id] = true;
         }
 
         emit NewDonation(id, msg.value);
@@ -104,17 +126,16 @@ contract Donation is Ownable {
     /// @notice Withdraw money from a campaign that is complete
     /// @dev The campaign must have the moneyGoal or timeGoal met in order to withdraw.
     /// @param id The id of the campaign we want to withdraw from
-    function withdraw(uint256 id) external campaignNotLocked(id) registered(id) {
+    function withdraw(uint256 id) external registered(id) lock(id) onlyCreator(id) {
         // todo owner is the person who created the campaign, no? Maybe update this modifier
         Campaign storage campaign = campaigns[id];
-        if (msg.sender != campaigns[id].creator) revert unauthorized();
 
         /* no need to check for moneyGoal, since the campaign will be marked
         as complete: true if a calling donate() caused the moneyGoal to be reached. */
         if (campaign.timeGoal <= block.timestamp) {
-            campaign.complete = true;
+            campaignComplete[id] = true;
         }
-        if (campaign.complete == false) revert ActiveCampaign();
+        if (campaignComplete[id] == false) revert ActiveCampaign();
 
         uint256 balance = campaignBalances[id];
         campaignBalances[id] = 0;
@@ -125,11 +146,15 @@ contract Donation is Ownable {
         emit FundsWithdrawn(id, balance);
     }
 
-    function lockCampaign(uint256 id) external onlyOwner {
+    function lockCampaign(uint256 id) external registered(id) lock(id) onlyOwner {
         campaingLocked[id] = true;
+        emit CampaignLocked(id);
     }
 
-    function unlockCampaign(uint256 id) external onlyOwner {
+    function unlockCampaign(uint256 id) external registered(id) onlyOwner {
+        if (campaingLocked[id] == false) revert campaignAlreadyUnlocked();
+
         campaingLocked[id] = false;
+        emit CampaignUnlocked(id);
     }
 }
